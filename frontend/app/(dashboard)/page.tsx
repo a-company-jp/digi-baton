51e62101da71ae3f26d15b7f6c3ed5b0feedc20a
pirotyyy <hiroki1423@docomo.ne.jp>
2025-03-02 02:02:13 +0900

    ✨ Clerkの導入

---
+++ frontend/app/(dashboard)/page.tsx
@@ -0,0 +1,12 @@
+import { SignOutButton } from "@clerk/nextjs";
+
+export default function HomePage() {
+  return (
+    <main>
+      <section>
+        Hello World
+      </section>
+      <SignOutButton />
+    </main>
+  )
+}
