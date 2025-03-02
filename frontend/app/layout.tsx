b018c6f6ef28d670c951a1be80c0cda77657a4af
Shion Ichikawa <shion1305@gmail.com>
2025-03-03 00:21:57 +0900

    🔥 remove unused lib and db on frontend side

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,32 +1,37 @@
-import { type Metadata, Viewport } from 'next'
+import { type Metadata, Viewport } from "next";
 import {
   ClerkProvider,
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   SignInButton,
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   SignUpButton,
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   SignedIn,
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   SignedOut,
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   UserButton,
-} from '@clerk/nextjs'
-import { Noto_Sans_JP } from 'next/font/google'
-import './globals.css'
+} from "@clerk/nextjs";
+import { Noto_Sans_JP } from "next/font/google";
+import "./globals.css";
 
 export const viewport: Viewport = {
   maximumScale: 1,
 };
 
 const notoSansJP = Noto_Sans_JP({
-  subsets: ['latin'],
+  subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
-  title: 'Digi Baton',
-  description: 'Digi Baton',
-}
+  title: "Digi Baton",
+  description: "Digi Baton",
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
     <html lang="en">
@@ -36,5 +41,5 @@ export default function RootLayout({
         </body>
       </ClerkProvider>
     </html>
-  )
+  );
 }
