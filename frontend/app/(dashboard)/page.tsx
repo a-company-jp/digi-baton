cd96768b95374bd4e9f8c7aff361c72c9cbce282
pirotyyy <hiroki1423@docomo.ne.jp>
2025-03-05 02:11:36 +0900

    ✨ ダッシュボードページの作成

--- frontend/app/(dashboard)/page.tsx
+++ frontend/app/(dashboard)/page.tsx
@@ -1,34 +1,42 @@
 import Image from "next/image";
+import AccountCard from "./account-card";
+import { SubscriptionCard } from "./subscription-card";
+import { DeviceCard } from "./device-card";
 
 export default function HomePage() {
   return (
     <main className="flex-1 p-4 lg:p-8">
-      <div className="flex flex-col md:flex-row items-center gap-8 p-8 rounded-lg bg-blue-800">
-        <Image
-          src="/imgs/welcome_icon.png"
-          alt="Welcome"
-          width={150}
-          height={150}
-          className="object-contain"
-        />
-        <div>
-          <h1 className="text-white text-lg lg:text-2xl font-bold truncate">
-            <span className="hidden md:inline">
-              今すぐ連携し、もしものときに備えましょう。
-            </span>
-            <span className="md:hidden">もしものときに備えましょう。</span>
-          </h1>
-          <p className="mt-4 text-gray-100 line-clamp-2">
-            <span className="hidden md:inline">
-              <span className="font-bold">KeyPer</span>
-              を使ってアカウント情報やデバイス情報を入力することで、
-              もしものときに引き継ぎが迅速に行えます。
-            </span>
-            <span className="md:hidden">
-              あなたのアカウントやデバイスの情報を登録しましょう。
-            </span>
-          </p>
+      <div className="flex flex-col gap-8">
+        <div className="flex flex-col md:flex-row items-center gap-8 p-8 rounded-lg bg-blue-800">
+          <Image
+            src="/imgs/welcome_icon.png"
+            alt="Welcome"
+            width={150}
+            height={150}
+            className="object-contain"
+          />
+          <div>
+            <h1 className="text-white text-lg lg:text-2xl font-bold truncate">
+              <span className="hidden md:inline">
+                今すぐ連携し、もしものときに備えましょう。
+              </span>
+              <span className="md:hidden">もしものときに備えましょう。</span>
+            </h1>
+            <p className="mt-4 text-gray-100 line-clamp-2">
+              <span className="hidden md:inline">
+                <span className="font-bold">KeyPer</span>
+                を使ってアカウント情報やデバイス情報を入力することで、
+                もしものときに引き継ぎが迅速に行えます。
+              </span>
+              <span className="md:hidden">
+                あなたのアカウントやデバイスの情報を登録しましょう。
+              </span>
+            </p>
+          </div>
         </div>
+        <AccountCard />
+        <SubscriptionCard />
+        <DeviceCard />
       </div>
     </main>
   );
