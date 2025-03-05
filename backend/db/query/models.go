434e7ade4f4c027e0cd3a9062282f37a031fc478
Najah7 <najahmatsuo7@gmail.com>
2025-03-05 14:35:27 +0900

    🗃️ [Update] users sql

--- backend/db/query/models.go
+++ backend/db/query/models.go
@@ -8,34 +8,8 @@ import (
 	"github.com/jackc/pgx/v5/pgtype"
 )
 
-type Account struct {
-	ID              int32
-	AppTemplateID   pgtype.Int4
-	AppName         pgtype.Text
-	AppDescription  pgtype.Text
-	AppIconUrl      pgtype.Text
-	AccountUsername string
-	EncPassword     []byte
-	Memo            string
-	PlsDelete       bool
-	Message         string
-	PasserID        pgtype.UUID
-	TrustID         pgtype.Int4
-	IsDisclosed     bool
-	CustomData      []byte
-}
-
-type Device struct {
-	ID                int32
-	DeviceType        int32
-	CredentialType    int32
-	DeviceDescription pgtype.Text
-	DeviceUsername    pgtype.Text
-	EncPassword       []byte
-	Memo              string
-	Message           string
-	PasserID          pgtype.UUID
-	TrustID           pgtype.Int4
-	IsDisclosed       bool
-	CustomData        []byte
+type User struct {
+	ID                pgtype.UUID
+	DefaultReceiverID pgtype.UUID
+	ClerkUserID       string
 }
--- backend/db/query/sql/passer_query.sql
+++
@@ -1,5 +0,0 @@
--- name: ListAccountsByPasserID :many
-SELECT * FROM accounts WHERE passer_id = $1;
-
--- name: ListDevicesByPasserID :many
-SELECT * FROM devices WHERE passer_id = $1;
