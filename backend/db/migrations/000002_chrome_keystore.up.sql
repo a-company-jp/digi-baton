50ac28ad8823799673fd307ec93cdc9eb3702632
Shion Ichikawa <shion1305@gmail.com>
2025-03-07 06:55:36 +0900

    🗃️ add constraint for duplicated user_ids in passkeys, add rp_id

--- backend/db/migrations/000002_chrome_keystore.up.sql
+++ backend/db/migrations/000002_chrome_keystore.up.sql
@@ -2,5 +2,7 @@ CREATE TABLE passkeys
 (
     id          SERIAL PRIMARY KEY,
     user_id     UUID  NOT NULL REFERENCES users (id),
-    private_key BYTEA NOT NULL
+    rp_id       TEXT  NOT NULL,
+    private_key BYTEA NOT NULL,
+    CONSTRAINT passkeys_user_id_rp_id_unique UNIQUE (user_id, rp_id)
 );
