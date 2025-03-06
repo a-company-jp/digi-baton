4785a70524a1581e68a86f59f9b11068684ecc63
Shion Ichikawa <shion1305@gmail.com>
2025-03-07 07:42:21 +0900

    🗃️ update queries and migration for passkeys

--- backend/db/query/sql/passkeys.mut.sql
+++ backend/db/query/sql/passkeys.mut.sql
@@ -1,9 +1,22 @@
--- name: SavePasskey :one
+-- name: CreatePasskey :one
 INSERT INTO passkeys (user_id,
                       rp_id,
-                      private_key)
+                      credential_id,
+                      user_name,
+                      public_key,
+                      private_key,
+                      sign_count)
 VALUES ($1, -- user_id
         $2, -- rp_id
-        $3 -- private_key
+        $3, -- credential_id
+        $4, -- user_name
+        $5, -- public_key
+        $6, -- private_key
+        $7 -- sign_count
        )
-RETURNING id, user_id, rp_id, private_key;
+RETURNING id, user_id, rp_id, credential_id, user_name, public_key, private_key, sign_count;
+
+-- name: UpdateSignCount :exec
+UPDATE passkeys
+SET sign_count = $2
+WHERE credential_id = $1;
--- backend/db/query/sql/passkeys.query.sql
+++ backend/db/query/sql/passkeys.query.sql
@@ -1,8 +1,24 @@
--- name: GetPasskeyByUserAndRp :one
+-- name: GetPasskeysByUserAndRp :one
 SELECT id,
        user_id,
        rp_id,
-       private_key
+       credential_id,
+       user_name,
+       public_key,
+       private_key,
+       sign_count
 FROM passkeys
 WHERE user_id = $1
   AND rp_id = $2;
+
+-- name: GetPasskeysByUserID :many
+SELECT id,
+       user_id,
+       rp_id,
+       credential_id,
+       user_name,
+       public_key,
+       private_key,
+       sign_count
+FROM passkeys
+WHERE user_id = $1;
